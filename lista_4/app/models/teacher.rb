bda48d251962766fad3fec1090215fbd81b51016
Victhor-Alexis <victhoralexis123@gmail.com>
2021-10-03 23:22:32 -0300

    Questao_2

--- lista_4/app/models/teacher.rb
+++ lista_4/app/models/teacher.rb
@@ -1,4 +1,7 @@
 class Teacher < ApplicationRecord
+    has_one :student, dependent: :destroy
+    has_one :language, dependent: :destroy
+
     validates :name, :birthdate, :email, presence: true
     validates :email, uniqueness: true
     validates_date :birthdate, on_or_before: lambda {Date.current}
--- lista_4/db/seeds.rb
+++ lista_4/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+prof_id = 0
+
+5.times do |i|
+    if i < 2
+        puts "Cadastrando professor #{i+1}"
+        Teacher.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, birthdate: Faker::Date.backward(days: 20000))
+        
+        prof_id = Teacher.first.id
+
+        puts "Cadastrando linguagem #{i+1}"
+        Language.create(name: Faker::ProgrammingLanguage.unique.name, teacher_id: prof_id)
+    end
+    
+    if i == 2
+        ++prof_id
+    end
+
+    puts "Cadastrando aluno #{i+1}"
+    Student.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, registration_number: Faker::Number.leading_zero_number(digits: 9), birthdate: Faker::Date.backward(days: 20000), teacher_id: prof_id)
+end
